3880405ef0919e8946af6ed6477250bb6584de78
Marcelo Viana <marcelorsviana@gmail.com>
2024-05-16 14:16:03 -0300

    Adicionar um arquivo de texto com as queries solicitadas pelo projeto.

---
+++ SQLQuery_Realizadas-Projeto.sql
@@ -0,0 +1,76 @@
+-- 1
+
+SELECT Nome, Ano FROM Filmes
+
+
+-- 2
+
+SELECT Nome, Ano, Duracao FROM Filmes
+ORDER BY Ano
+
+
+-- 3
+
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+
+-- 4
+
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Ano = 1997
+
+
+-- 5
+
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Ano > 2000
+
+
+-- 6
+
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+
+-- 7
+
+SELECT Ano, COUNT(Nome) Quantidade FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+
+-- 8
+
+SELECT * FROM Atores
+WHERE Genero = 'M'
+
+
+-- 9
+
+SELECT * FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+
+-- 10
+
+SELECT Nome, Genero FROM FilmesGenero
+INNER JOIN Filmes ON FilmesGenero.IdFilme = Filmes.Id
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+
+-- 11
+
+SELECT Nome, Genero FROM FilmesGenero
+INNER JOIN Filmes ON FilmesGenero.IdFilme = Filmes.Id
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+
+-- 12
+
+SELECT Nome, PrimeiroNome, UltimoNome, Papel FROM ElencoFilme
+INNER JOIN Atores ON ElencoFilme.IdAtor = Atores.id
+INNER join Filmes ON ElencoFilme.IdFilme = Filmes.id
